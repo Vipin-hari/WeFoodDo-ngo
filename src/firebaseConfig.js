22bb60f2a72d4bfe1bc3dc6c7c6a168ea3d2282e
Vipin-hari <84123617+Vipin-hari@users.noreply.github.com>
2024-08-08 19:33:38 +0530

    Updated api key values in firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYOTd2HPKE7cw_mEv6QYXBRXXhvpM9H9Y",
-  authDomain: "foodo-f1881.firebaseapp.com",
-  projectId: "foodo-f1881",
-  storageBucket: "foodo-f1881.appspot.com",
-  messagingSenderId: "324458471747",
-  appId: "1:324458471747:web:003d31d611ea77b16d4ff6",
-  measurementId: "G-KE20V8B8ST"
-};
+    apiKey: "nononononononono",
+    authDomain: "nonono",
+    projectId: "foodo-f1881",
+    storageBucket: "foodo-f1881.appspot.com",
+    messagingSenderId: "324458471747",
+    appId: "nonononono",
+    measurementId: "G-KE20V8B8ST"
+  };
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
