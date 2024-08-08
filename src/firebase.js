2bcb9d153f078fe2144580fb3f4b3bb4497702ba
Vipin-hari <84123617+Vipin-hari@users.noreply.github.com>
2024-08-08 19:33:12 +0530

    Updated api key values firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDYOTd2HPKE7cw_mEv6QYXBRXXhvpM9H9Y",
-    authDomain: "foodo-f1881.firebaseapp.com",
+    apiKey: "nononononononono",
+    authDomain: "nonono",
     projectId: "foodo-f1881",
     storageBucket: "foodo-f1881.appspot.com",
     messagingSenderId: "324458471747",
-    appId: "1:324458471747:web:003d31d611ea77b16d4ff6",
+    appId: "nonononono",
     measurementId: "G-KE20V8B8ST"
   };
 
