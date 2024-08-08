1fc60dd5608fcde94d065b9da406acbf09850e33
vipin.g2020@kgkite.ac.in <vipin.g2020@kgkite.ac.in>
2024-08-08 19:17:23 +0530

    Updated the foldre structure

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYOTd2HPKE7cw_mEv6QYXBRXXhvpM9H9Y",
+    authDomain: "foodo-f1881.firebaseapp.com",
+    projectId: "foodo-f1881",
+    storageBucket: "foodo-f1881.appspot.com",
+    messagingSenderId: "324458471747",
+    appId: "1:324458471747:web:003d31d611ea77b16d4ff6",
+    measurementId: "G-KE20V8B8ST"
+  };
+
+  
+  export default firebaseConfig;
